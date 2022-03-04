e8cb9d780b51d8940a3d8c7108c279125fb695f1
MaximeLeduey <91879887+MaximeLeduey@users.noreply.github.com>
2022-03-04 13:21:24 +0100

    functions

---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php 
+
+const api_key = 'api_key=741c99a9094ee27f1151d179c1dea765';
+const requestStart = "https://api.themoviedb.org/3";
+
+
+?>
--- functions.php
+++ functions.php
@@ -1,8 +1,26 @@
 <?php 
 
+require('./vendor/autoload.php');
+require('./config/config.php');
+use GuzzleHttp\Client;
+
+
 function get_client() {
-    
+    $client = new Client();
+    return $client;
 }
+
+function make_request(Client $client,string $url, string $key) {
+    $startUrl = $url;
+    $api_key = $key;
+    $response = $client->get("$startUrl/tv/1402/images?$api_key");
+    $result = $response->getBody()->getContents();
+    var_dump(json_decode($result, JSON_PRETTY_PRINT));
+    return json_decode($result);
+}
+
+$client = get_client();
+make_request($client, requestStart, api_key);
 
 
 
