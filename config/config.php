0b7c68d7aac3392dd5d0c2ff9e40a389b1f4c7a6
MaximeLeduey <91879887+MaximeLeduey@users.noreply.github.com>
2022-03-07 11:51:05 +0100

    functions.php

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php 
 
-const api_key = 'api_key=741c99a9094ee27f1151d179c1dea765';
+const api_key = '?api_key=741c99a9094ee27f1151d179c1dea765';
 const requestStart = "https://api.themoviedb.org/3";
 
 
--- functions.php
+++ functions.php
@@ -1,27 +1,53 @@
 <?php 
 
+function pretty_print_r($var): void {
+    echo '<pre>' . print_r($var, true) . '</pre>';
+}
+
+
 require('./vendor/autoload.php');
 require('./config/config.php');
 use GuzzleHttp\Client;
 
 
 function get_client() {
-    $client = new Client();
+    $client = new Client([
+        'verify' => false,
+        'stream' => false,
+    ]);
     return $client;
 }
 
-function make_request(Client $client,string $url, string $key) {
-    $startUrl = $url;
-    $api_key = $key;
-    $response = $client->get("$startUrl/tv/1402/images?$api_key");
+function make_request(Client $client,string $url) {
+    $response = $client->get($url);
     $result = $response->getBody()->getContents();
-    var_dump(json_decode($result, JSON_PRETTY_PRINT));
-    return json_decode($result);
+    // pretty_print_r($result);
+    $resultObject = json_decode($result);
+    $result = $resultObject->results;
+    return $result;
 }
 
+function get_film_by_id() {
+    $client = get_client();
+    return make_request($client, requestStart.'/movie/popular/'.api_key);
+}
 $client = get_client();
-make_request($client, requestStart, api_key);
 
+get_film_by_id();
+
+
+function get_Infos(array $array) {
+    foreach($array as $film) {
+        $image = $film->poster_path;
+        $title = $film->original_title;
+        $overview = $film->overview;
+        echo "$title $image $overview<br>";
+    }
+}
+
+
+
+get_Infos(get_film_by_id());
 
 
 
